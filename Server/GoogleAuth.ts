a4e86e2a21f9f3b4bdbf1cac5f181c742f228dbc
wildbirdStan <stan.johler@wildbirdtrust.org>
2025-02-19 23:26:36 +0000

    GoogleAuth.ts file created

---
+++ Server/GoogleAuth.ts
@@ -0,0 +1,18 @@
+// import { JWT } from "google-auth-library";
+// import creds from "./coast-salish-plant-nursery-app-7cc80564577b.json";
+
+// const SCOPES = [
+//     'https://www.googleapis.com/auth/spreadsheets',
+//     'https://www.googleapis.com/auth/drive.file',
+//   ];
+
+// export async function createTokenAsync() {
+//     const token = new JWT({
+//     email: creds.client_email,
+//     key: creds.private_key,
+//     scopes: SCOPES,
+//     })
+//     return token;
+// }
+
+

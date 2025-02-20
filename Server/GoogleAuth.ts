b2c71822c9f8e17bb897f1f76390c6892512c78d
wildbirdStan <stan.johler@wildbirdtrust.org>
2025-02-20 18:21:27 +0000

    Services and Auth file edited

--- Server/GoogleAuth.ts
+++ Server/GoogleAuth.ts
@@ -1,18 +1,18 @@
-// import { JWT } from "google-auth-library";
-// import creds from "./coast-salish-plant-nursery-app-7cc80564577b.json";
+import { JWT } from "google-auth-library";
+import creds from "./coast-salish-plant-nursery-app-7cc80564577b.json";
 
-// const SCOPES = [
-//     'https://www.googleapis.com/auth/spreadsheets',
-//     'https://www.googleapis.com/auth/drive.file',
-//   ];
+const SCOPES = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive.file',
+  ];
 
-// export async function createTokenAsync() {
-//     const token = new JWT({
-//     email: creds.client_email,
-//     key: creds.private_key,
-//     scopes: SCOPES,
-//     })
-//     return token;
-// }
+export async function createTokenAsync() {
+    const token = new JWT({
+    email: creds.client_email,
+    key: creds.private_key,
+    scopes: SCOPES,
+    })
+    return token;
+}
 
 

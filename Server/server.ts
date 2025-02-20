0b535d469257e1f5998ca67e3481dec22d120fff
wildbirdStan <stan.johler@wildbirdtrust.org>
2025-02-20 22:20:16 +0000

    errors fixed in server.ts

--- Server/server.ts
+++ Server/server.ts
@@ -1,37 +1,37 @@
-// import express from "express";
-// const app = express();
-// import { getFilteredPlantsAsync, getPlantsAsync } from "./services";
-// import { Plant } from "./Plant";
-// import { PlantType } from "./Enums";
+import express from "express";
+const app = express();
+import { getFilteredPlantsAsync, getPlantsAsync } from "./services";
+import { Plant } from "./Types/Plant";
+import { PlantType } from "./Enums/Enums";
 
 
 
-// app.listen(3000, () => {
-//     console.log("App is running on port 3000");
-// })
+app.listen(3000, () => {
+    console.log("App is running on port 3000");
+})
 
-// app.get("/api", async (req, res) => {
-//     const plants = await getPlantsAsync();
+app.get("/api", async (req, res) => {
+    const plants = await getPlantsAsync();
 
     
-//     Array
-//     if (plants.length != 0) {
-//         res.setHeader("content-type", "application/json");
-//         res.status(200).send(JSON.stringify(plants));
-//     } else {
-//         res.send(JSON.stringify({Error: "Bad Request"})).sendStatus(400);
-//     }
-// })
+    Array
+    if (plants.length != 0) {
+        res.setHeader("content-type", "application/json");
+        res.status(200).send(JSON.stringify(plants));
+    } else {
+        res.send(JSON.stringify({Error: "Bad Request"})).sendStatus(400);
+    }
+})
 
-// app.get("/test", async (req, res) => {
-//     const plants = await getFilteredPlantsAsync({
-//         PlantType: [PlantType.Annual, PlantType.Fern],
+app.get("/test", async (req, res) => {
+    const plants = await getFilteredPlantsAsync({
+        PlantType: [PlantType.Annual, PlantType.Fern],
          
-//     });
-//     if (plants.length != 0) {
-//         res.setHeader("content-type", "application/json");
-//         res.status(200).send(JSON.stringify(plants));
-//     } else {
-//         res.send(JSON.stringify({Error: "Bad Request"})).sendStatus(400);
-//     }
-// })
+    });
+    if (plants.length != 0) {
+        res.setHeader("content-type", "application/json");
+        res.status(200).send(JSON.stringify(plants));
+    } else {
+        res.send(JSON.stringify({Error: "Bad Request"})).sendStatus(400);
+    }
+})

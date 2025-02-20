22639eaaa2977a3d90b778b249fc400c24e3ba14
wildbirdStan <stan.johler@wildbirdtrust.org>
2025-02-20 18:23:26 +0000

    Enum File created

---
+++ Server/Enums/Enums.ts
@@ -0,0 +1,48 @@
+export enum PlantType {
+    "Annual",
+    "Fern",
+    "Grass",
+    "Perennial",
+    "Shrub",
+    "Tree"
+}
+
+export enum Exposure {
+    "Part Shade",
+    "Part Shade - Shade",
+    "Sun",
+    "Sun - Part Shade",
+    "Sun - Shade"
+}
+
+export enum Soil {
+    "Light (Sandy) - Heavy (Clay)",
+    "Light (Sandy) - Heavy (Clay) / Well-drained",
+    "Light (Sandy) - Medium (Loamy)",
+    "Light (Sandy) - Medium (Loamy) / Well-drained",
+    "Medium (Loamy) - Heavy (Clay)"
+}
+
+export enum Moisture {
+    "Dry",
+    "Dry - Moist",
+    "Dry - Wet",
+    "Moist",
+    "Moist - Dry",
+    "Moist - Wet",
+    "Wet"
+}
+
+export enum ContainerTolerance {
+    "High",
+    "Low",
+    "Medium",
+    "Moderate"
+}
+
+export enum Attracts {
+    "Bees and Butterflies",
+    "Birds",
+    "Hummingbirds"
+}
+

0a8eb0eff026a7e73a0a93b9b59a63df5ab3ed7a
wildbirdStan <stan.johler@wildbirdtrust.org>
2025-02-25 20:02:28 +0000

    services.ts filter updated

--- Server/dist/services.js
+++ Server/dist/services.js
@@ -58,18 +58,18 @@ function getFilteredPlantsAsync(creteria) {
         let plants = yield getPlantsAsync();
         let filteredPlants = plants.filter(item => {
             var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l;
-            return (((_a = creteria.PlantType) === null || _a === void 0 ? void 0 : _a.length) == 0 ? true : (((_b = creteria.PlantType) === null || _b === void 0 ? void 0 : _b.includes(item.PlantType)) ? true : false)) &&
-                (((_c = creteria.Soil) === null || _c === void 0 ? void 0 : _c.length) == 0 ? true : (((_d = creteria.Soil) === null || _d === void 0 ? void 0 : _d.includes(item.Soil)) ? true : false)) &&
+            return (((_a = creteria.Soil) === null || _a === void 0 ? void 0 : _a.length) == 0 ? true : (((_b = creteria.Soil) === null || _b === void 0 ? void 0 : _b.includes(item.Soil)) ? true : false)) &&
+                (((_c = creteria.PlantType) === null || _c === void 0 ? void 0 : _c.length) == 0 ? true : (((_d = creteria.PlantType) === null || _d === void 0 ? void 0 : _d.includes(item.PlantType)) ? true : false)) &&
                 (((_e = creteria.Exposure) === null || _e === void 0 ? void 0 : _e.length) == 0 ? true : (((_f = creteria.Exposure) === null || _f === void 0 ? void 0 : _f.includes(item.Exposure)) ? true : false)) &&
                 (((_g = creteria.Moisture) === null || _g === void 0 ? void 0 : _g.length) == 0 ? true : (((_h = creteria.Moisture) === null || _h === void 0 ? void 0 : _h.includes(item.Moisture)) ? true : false)) &&
                 (((_j = creteria.ContainerTolerance) === null || _j === void 0 ? void 0 : _j.length) == 0 ? true : (((_k = creteria.ContainerTolerance) === null || _k === void 0 ? void 0 : _k.includes(item.ContainerTolerance)) ? true : false)) &&
-                (((_l = creteria.Attracts) === null || _l === void 0 ? void 0 : _l.length) == 0 ? true : (arrayEntryInText(creteria.Attracts, item.Attracts.toString()) == true ? true : false));
+                (((_l = creteria.Attracts) === null || _l === void 0 ? void 0 : _l.length) == 0 ? true : (arrayEntryInText(creteria.Attracts, (item.Attracts === undefined ? "" : item.Attracts.toString())) == true ? true : false));
         });
         console.log(filteredPlants.length);
         return filteredPlants;
     });
 }
-function arrayEntryInText(array, text) {
+function arrayEntryInText(array, text = "") {
     let result = false;
     let textToLowerCase = text.toLowerCase();
     array.forEach(element => {
--- Server/services.ts
+++ Server/services.ts
@@ -46,12 +46,13 @@ finally {return plants}
 export async function getFilteredPlantsAsync(creteria: Criteria) {
     let plants = await getPlantsAsync();
     let filteredPlants = plants.filter(item => {
-        return (creteria.PlantType?.length == 0 ? true : (creteria.PlantType?.includes(item.PlantType) ? true : false)) && 
-                (creteria.Soil?.length == 0 ? true : (creteria.Soil?.includes(item.Soil) ? true : false) ) &&
+        return  (creteria.Soil?.length == 0 ? true : (creteria.Soil?.includes(item.Soil) ? true : false)) &&
+                (creteria.PlantType?.length == 0 ? true : (creteria.PlantType?.includes(item.PlantType) ? true : false)) &&
                 (creteria.Exposure?.length == 0 ? true : (creteria.Exposure?.includes(item.Exposure) ? true : false)) && 
                 (creteria.Moisture?.length == 0 ? true : (creteria.Moisture?.includes(item.Moisture) ? true : false)) && 
                 (creteria.ContainerTolerance?.length == 0 ? true : (creteria.ContainerTolerance?.includes(item.ContainerTolerance) ? true : false)) &&
-                (creteria.Attracts?.length == 0 ? true : (arrayEntryInText(<Array<string>>creteria.Attracts, item.Attracts.toString()) == true ? true : false))    
+                (creteria.Attracts?.length == 0 ? true : (arrayEntryInText(<Array<string>>creteria.Attracts, (item.Attracts === undefined ? "" : item.Attracts.toString())) == true ? true : false)) 
+                
     })
     console.log(filteredPlants.length)
     return filteredPlants;
@@ -59,7 +60,7 @@ export async function getFilteredPlantsAsync(creteria: Criteria) {
 
 
 
-function arrayEntryInText(array: String[], text: string) {
+function arrayEntryInText(array: String[], text: string = "") {
     let result = false;
     let textToLowerCase = text.toLowerCase();
     array.forEach(element => {

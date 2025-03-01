f1125f982cfc802067794867c75d3709e12387af
wildbirdStan <stan.johler@wildbirdtrust.org>
2025-03-01 11:10:38 -0800

    globalStyles.ts created

---
+++ Client/coast-salish-plant-nursery-app/src/styles/globalStyles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  fontFamily: {
+    fontFamily: "DM Sans",
+  },
+
+  centeredText: {
+    textAlign: "center",
+  },
+});

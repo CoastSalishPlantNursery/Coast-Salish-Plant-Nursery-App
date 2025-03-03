d6b97d85d246ef090c932cf781dde9285edc6e4c
wildbirdStan <stan.johler@wildbirdtrust.org>
2025-03-03 10:49:28 -0800

    addSelectedItems and removeSelectedItem properties added

--- Client/coast-salish-plant-nursery-app/src/components/CheckbuttonGroup.tsx
+++ Client/coast-salish-plant-nursery-app/src/components/CheckbuttonGroup.tsx
@@ -3,14 +3,26 @@ import Checkbutton from "./Checkbutton";
 
 type CheckButtonGroupProps = {
   selections: string[];
+  selectedItems: string[];
+  addSelectedItem: (selectedItem: string) => void;
+  removeSelectedItem: (selectedItem: string) => void;
 };
 
 export default function CheckbuttonGroup(props: CheckButtonGroupProps) {
+  const { selections, selectedItems, addSelectedItem, removeSelectedItem } =
+    props;
+
   return (
     <>
       <View style={styles.container}>
         {props.selections.map((item) => (
-          <Checkbutton key={item} title={item} checked={() => {}} />
+          <Checkbutton
+            key={item}
+            title={item}
+            checked={(isChecked: boolean) => {
+              isChecked ? removeSelectedItem(item) : addSelectedItem(item);
+            }}
+          />
         ))}
       </View>
     </>

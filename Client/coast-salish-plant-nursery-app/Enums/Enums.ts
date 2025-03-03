dd59e0967f4add2d2dc5bca1a515eda65fec0300
wildbirdStan <stan.johler@wildbirdtrust.org>
2025-03-03 09:20:15 -0800

    Enums.ts created

---
+++ Client/coast-salish-plant-nursery-app/Enums/Enums.ts
@@ -0,0 +1,48 @@
+export enum PlantType {
+  "Annual" = "Annual",
+  "Fern" = "Fern",
+  "Grass" = "Grass",
+  "Perennial" = "Perennial",
+  "Shrub" = "Shrub",
+  "Tree" = "Tree",
+}
+
+export enum Exposure {
+  "Part shade" = "Part shade",
+  "Part shade - shade" = "Part shade - Shade",
+  "Sun" = "Sun",
+  "Sun - Part shade" = "Sun - Part shade",
+  "Sun - Shade" = "Sun - Shade",
+}
+
+export enum Soil {
+  "Light (Sandy) - Heavy (Clay)" = "Light (Sandy) - Heavy (Clay)",
+  "Light (Sandy) - Heavy (Clay) / Well-drained" = "Light (Sandy) - Heavy (Clay) / Well-drained",
+  "Light (Sandy) - Medium (Loamy)" = "Light (Sandy) - Medium (Loamy)",
+  "Light (Sandy) - Medium (Loamy) / Well-drained" = "Light (Sandy) - Medium (Loamy) / Well-drained",
+  "Medium (Loamy) - Heavy (Clay)" = "Medium (Loamy) - Heavy (Clay)",
+}
+
+export enum Moisture {
+  "Dry" = "Dry",
+  "Dry - Moist" = "Dry - Moist",
+  "Dry - Wet" = "Dry - Wet",
+  "Moist" = "Moist",
+  "Moist - Dry" = "Moist - Dry",
+  "Moist - Wet" = "Moist - Wet",
+  "Wet" = "Wet",
+}
+
+export enum ContainerTolerance {
+  "High" = "High",
+  "Low" = "Low",
+  "Medium" = "Medium",
+  "Moderate" = "Moderate",
+}
+
+export enum Attracts {
+  "Bees" = "Bees",
+  "Butterflies" = "Butterflies",
+  "Birds" = "Birds",
+  "Hummingbirds" = "Hummingbirds",
+}

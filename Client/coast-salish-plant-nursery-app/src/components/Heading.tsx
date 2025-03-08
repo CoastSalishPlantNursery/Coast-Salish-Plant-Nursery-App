53c7f0c5e3dc971db71cfc3a33ec9c8ac2493a46
wildbirdStan <stan.johler@wildbirdtrust.org>
2025-03-08 13:50:26 -0800

    Heading.tsx space added

--- Client/coast-salish-plant-nursery-app/src/components/Heading.tsx
+++ Client/coast-salish-plant-nursery-app/src/components/Heading.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SafeAreaView, StatusBar, Platform, Text, View } from "react-native";
 import { globalStyles } from "../styles/globalStyles";
+
 export enum TextStyle {
   regular = "regular",
   bold = "bold",

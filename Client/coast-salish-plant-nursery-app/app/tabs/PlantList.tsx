98d52a7a6d9bfcb3161d24c3235eb9800511eaab
wildbirdStan <stan.johler@wildbirdtrust.org>
2025-03-17 14:46:23 -0700

    PlantList.tsx updated

--- Client/coast-salish-plant-nursery-app/app/tabs/PlantList.tsx
+++ Client/coast-salish-plant-nursery-app/app/tabs/PlantList.tsx
@@ -1,4 +1,13 @@
-import { StyleSheet, View, Text, FlatList, Alert } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  Alert,
+  ActivityIndicator,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
 import { globalStyles } from "../../src/styles/globalStyles";
 import { useEffect, useState } from "react";
 import { router, useLocalSearchParams } from "expo-router";
@@ -8,18 +17,23 @@ import ListItem from "../../src/components/ListItem";
 import Heading, { HeadingSize, TextStyle } from "../../src/components/Heading";
 import { Criteria } from "../../Types/Criteria";
 import BackButton from "../../src/components/BackButton";
+import MessageCard from "../../src/components/MessageCard";
 
 type Props = {};
 
 export default function PlantList({}: Props) {
   const [plants, setPlants] = useState<Plant[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [fetchError, setFetchError] = useState<string | null>(null);
   const { searchBarValue, criteria } = useLocalSearchParams();
 
   useEffect(() => {
     const fetchPlants = async () => {
-      const criteriaToObject = JSON.parse(criteria.toString()) as Criteria;
-      console.log(criteriaToObject.PlantType);
+      const criteriaToObject =
+        criteria !== undefined
+          ? (JSON.parse(criteria.toString()) as Criteria)
+          : undefined;
+
       setIsLoading(true);
 
       const { data, error } = await (criteriaToObject !== undefined
@@ -36,9 +50,9 @@ export default function PlantList({}: Props) {
                 )
               )
         );
-        setIsLoading(false);
+        console.log("Pflanzen Länge " + plants.length);
       } else {
-        console.log(error.toString());
+        setFetchError(error);
         Alert.alert(
           "Error Message",
           "Failed to load Plants. Please check your internet connection and try again later.",
@@ -58,44 +72,67 @@ export default function PlantList({}: Props) {
           ]
         );
       }
+      setIsLoading(false);
     };
+
     fetchPlants();
   }, []);
+
   return (
     <>
-      <View style={globalStyles.margin}>
-        <View style={styles.backButton}>
-          <BackButton onClick={() => router.back()} />
+      {isLoading ? (
+        <View style={styles.fullWidthContainer}>
+          <ActivityIndicator size={"large"} color={"#52B788"} />
         </View>
-        <View style={styles.heading}>
-          <Heading
-            text="Matching Plants"
-            heading={HeadingSize.h2}
-            style={TextStyle.regular}
-          />
+      ) : fetchError != null ? (
+        <MessageCard message="An Error occured. Please restart the App and try again" />
+      ) : (
+        <View style={globalStyles.margin}>
+          <View style={styles.backButton}>
+            <BackButton onClick={() => router.back()} />
+          </View>
+          <View style={styles.heading}>
+            <Heading
+              text="Matching Plants"
+              heading={HeadingSize.h2}
+              style={TextStyle.regular}
+            />
+          </View>
+          {plants.length == 0 || plants.length === undefined ? (
+            <MessageCard message="No Plants matching with the Criteria provided." />
+          ) : (
+            <FlatList
+              style={styles.list}
+              data={plants}
+              renderItem={(element) => {
+                return (
+                  <ListItem
+                    PlantName={element.item.PlantName}
+                    onClick={() => {
+                      console.log("Clicked");
+                      router.push({
+                        pathname: "/tabs/PlantOverview",
+                        params: {
+                          PlantName: element.item.PlantName,
+                          LatinName: element.item.LatinName,
+                          PlantType: element.item.PlantType,
+                          Exposure: element.item.Exposure,
+                          Soil: element.item.Soil,
+                          Moisture: element.item.Moisture,
+                          ContainerTolerance: element.item.ContainerTolerance,
+                          Attracts: element.item.Attracts,
+                          Summary: element.item.Summary,
+                          Tags: element.item.Tags,
+                        },
+                      });
+                    }}
+                  />
+                );
+              }}
+            />
+          )}
         </View>
-
-        <FlatList
-          data={plants}
-          renderItem={(element) => {
-            return (
-              <ListItem
-                PlantName={element.item.PlantName}
-                onClick={() =>
-                  router.push({
-                    pathname: "/tabs/PlantOverview",
-                    params: {
-                      PlantName: element.item.PlantName,
-                      LatinName: element.item.LatinName,
-                      Summary: element.item.Summary,
-                    },
-                  })
-                }
-              />
-            );
-          }}
-        />
-      </View>
+      )}
     </>
   );
 }
@@ -107,5 +144,17 @@ const styles = StyleSheet.create({
 
   heading: {
     marginBottom: 25,
+  },
+
+  fullWidthContainer: {
+    display: "flex",
+    flexGrow: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+
+  list: {
+    height: "85%",
   },
 });

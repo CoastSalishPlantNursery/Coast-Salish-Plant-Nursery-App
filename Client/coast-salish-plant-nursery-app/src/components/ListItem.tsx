05bb0b2738ab58c8134bed0601a849f8c0f9d732
wildbirdStan <stan.johler@wildbirdtrust.org>
2025-05-21 13:07:52 -0700

    ListItem.tsx style classes added

--- Client/coast-salish-plant-nursery-app/src/components/ListItem.tsx
+++ Client/coast-salish-plant-nursery-app/src/components/ListItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyleSheet, View, Text, TouchableWithoutFeedback } from "react-native";
+import { globalStyles } from "../styles/globalStyles";
 
 type ListItemProps = {
   PlantName: string;
@@ -10,8 +11,8 @@ export default function ListItem(props: ListItemProps) {
   const { PlantName, onClick } = props;
   return (
     <TouchableWithoutFeedback onPress={onClick}>
-      <View style={styles.item}>
-        <Text>{PlantName}</Text>
+      <View style={[styles.item, globalStyles.primaryBackgroundColor]}>
+        <Text style={{ color: "green" }}>{PlantName}</Text>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -21,12 +22,12 @@ const styles = StyleSheet.create({
   item: {
     display: "flex",
     justifyContent: "center",
+    alignItems: "center",
     width: "100%",
     height: 42,
     paddingLeft: 20,
     marginBottom: 10,
-    backgroundColor: "lightgrey",
-    opacity: 0.6,
+    // opacity: 0.6,
 
     borderRadius: 5,
   },

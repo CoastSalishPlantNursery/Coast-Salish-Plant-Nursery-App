9842e1fb66374570f8c43fbef9178b3d617a1372
wildbirdStan <stan.johler@wildbirdtrust.org>
2025-05-21 13:14:37 -0700

    globalStyles.ts styling classes added

--- Client/coast-salish-plant-nursery-app/src/styles/globalStyles.ts
+++ Client/coast-salish-plant-nursery-app/src/styles/globalStyles.ts
@@ -1,8 +1,32 @@
 import { StyleSheet } from "react-native";
 
 export const globalStyles = StyleSheet.create({
-  fontFamily: {
-    fontFamily: "DM Sans",
+  primaryHeadingColor: {
+    color: "#40916C",
+  },
+
+  secondaryHeadingColor: {
+    color: "#52B788",
+  },
+
+  primaryBackgroundColor: {
+    backgroundColor: "#D8F3DC",
+  },
+
+  secondaryTextColor: {
+    color: "#95D5B2",
+  },
+
+  primaryButtonColor: {
+    backgroundColor: "#2D6A4F",
+  },
+
+  secondaryButtonColor: {
+    backgroundColor: "#74C69D",
+  },
+
+  primaryBorderColor: {
+    borderColor: "#1B4342",
   },
 
   centeredText: {
